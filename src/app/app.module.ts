2acd2452842ebe28c7f40be8037fb2d7fe694cd3
cmdevendran <cmdevendran@gmail.com>
2017-05-23 14:40:43 +0800

    working upto firebase

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+apiKey: "AIzaSyDERusHhi9a7-1OBcZO3NDkN3kDZL4Gs-o",
+   authDomain: "todo-61998.firebaseapp.com",
+   databaseURL: "https://todo-61998.firebaseio.com",
+   projectId: "todo-61998",
+   storageBucket: "todo-61998.appspot.com",
+   messagingSenderId: "926728889067"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+
+export class HomePage {
+songs: FirebaseListObservable<any>;
+
+
+constructor(public navCtrl: NavController, af: AngularFire) {
+this.songs = af.database.list('/songs');
+}
+
+}
